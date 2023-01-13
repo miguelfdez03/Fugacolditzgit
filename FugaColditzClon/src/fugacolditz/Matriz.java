9137ef5434e91dc28536e968a8d3cc3136d85f0a
miguel <mfm0015@alu.medac.es>
2023-01-13 18:55:41 +0100

    Mover guardias con recursividad ya funciona perfecto no se come las herramientas!!!

--- FugaColditzClon/src/fugacolditz/Matriz.java
+++ FugaColditzClon/src/fugacolditz/Matriz.java
@@ -567,31 +567,47 @@ public class Matriz {
         switch (ale) {
             case 1:
                 //arriba
+                if((matriz.tablero[g.x - 1][g.y].equalsIgnoreCase("U"))||(matriz.tablero[g.x - 1][g.y].equalsIgnoreCase("A"))
+                        ||(matriz.tablero[g.x - 1][g.y].equalsIgnoreCase("P"))||(matriz.tablero[g.x - 1][g.y].equalsIgnoreCase("G"))){
+                    moverG(matriz,g);
+                }else{
                 matriz.tablero[g.x - 1][g.y] = "G";
                 matriz.tablero[g.x][g.y] = "X";
-                g.x = g.x - 1;
+                g.x = g.x - 1;}
                 break;
             case 2:
                 //abajo
+                if((matriz.tablero[g.x + 1][g.y].equalsIgnoreCase("U"))||(matriz.tablero[g.x + 1][g.y].equalsIgnoreCase("A"))
+                        ||(matriz.tablero[g.x + 1][g.y].equalsIgnoreCase("P"))||(matriz.tablero[g.x + 1][g.y].equalsIgnoreCase("G"))){
+                    moverG(matriz,g);
+                }else{
                 matriz.tablero[g.x + 1][g.y] = "G";
                 matriz.tablero[g.x][g.y] = "X";
                 g.x = g.x + 1;
-                break;
+                break;}
             case 3:
                 //izquierda
+                if((matriz.tablero[g.x][g.y-1].equalsIgnoreCase("U"))||(matriz.tablero[g.x][g.y-1].equalsIgnoreCase("A"))
+                        ||(matriz.tablero[g.x][g.y-1].equalsIgnoreCase("P"))||(matriz.tablero[g.x][g.y-1].equalsIgnoreCase("G"))){
+                    moverG(matriz,g);
+                }else{
                 matriz.tablero[g.x][g.y - 1] = "G";
                 matriz.tablero[g.x][g.y] = "X";
                 g.y = g.y - 1;
-                break;
+                break;}
             case 4:
                 //derecha
+                if((matriz.tablero[g.x][g.y+1].equalsIgnoreCase("U"))||(matriz.tablero[g.x][g.y+1].equalsIgnoreCase("A"))
+                        ||(matriz.tablero[g.x][g.y+1].equalsIgnoreCase("P"))||(matriz.tablero[g.x][g.y+1].equalsIgnoreCase("G"))){
+                    moverG(matriz,g);
+                }else{
                 matriz.tablero[g.x][g.y + 1] = "G";
                 matriz.tablero[g.x][g.y] = "X";
-                g.y = g.y + 1;
+                g.y = g.y + 1;}
                 break;
         }
         }catch(ArrayIndexOutOfBoundsException e){
-            
+            moverG(matriz,g);
         }
     }
 
